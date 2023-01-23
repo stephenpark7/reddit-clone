34f99ccaae49557f1a1b9ec4184b413ec2116657
stephenpark7 <spark0843@gmail.com>
2023-01-22 19:49:26 -0500

    Add CSS for NotFound page

--- client/src/App/pages/NotFound/index.tsx
+++ client/src/App/pages/NotFound/index.tsx
@@ -1,6 +1,8 @@
+import styles from '../../styles/Home.module.css';
+
 export default function NotFound() {
   return (
-    <div className='page-container'>
+    <div className={styles.pageContainer}>
       <h1>Error 404</h1>
       <span>Page could not be found.</span>
     </div>
