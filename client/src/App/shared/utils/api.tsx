6ece4a0f036d0cc23a2578441ac8e278c0f11d62
Stephen Park <spark0843@gmail.com>
2023-08-16 01:37:32 -0400

    refacotr: api uto;s

--- client/src/App/shared/utils/api.tsx
+++ client/src/App/shared/utils/api.tsx
@@ -1,46 +1,31 @@
-export const signUp = async (username: string, password: string) => {
+const apiUrl = (endpoint: string): string => `${process.env.REACT_APP_API_URL}/api/user/${endpoint}`;
+
+const apiRequest = async (endpoint: string, body: any): Promise<any> => {
   try {
-    const response = await fetch
-    (`${process.env.REACT_APP_API_URL}/api/user/signup`, 
-      {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({'username': username, 'password': password}),
-      }
-    );
+    const response = await fetch(apiUrl(endpoint), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
     if (!response.ok) {
       throw new Error(await response.text());
     }
+
     const data = await response.json();
     return data;
-  } catch (err: any) {
+  } catch (err) {
     throw err;
   }
 };
 
-export const logIn = async (username: string, password: string) => {
-  try {
-    const response = await fetch
-    (`${process.env.REACT_APP_API_URL}/api/user/login`, 
-      {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({'username': username, 'password': password}),
-      }
-    );
-    if (!response.ok) {
-      throw new Error(await response.text());
-    }
-    const data = await response.json();
-    return data;
-  }
-  catch (err: any) {
-    throw err;
-  }
+export const signUp = async (username: string, password: string): Promise<any> => {
+  return apiRequest('signup', { username, password });
+};
+
+export const logIn = async (username: string, password: string): Promise<any> => {
+  return apiRequest('login', { username, password });
 };
